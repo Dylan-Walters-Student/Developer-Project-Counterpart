0ec69fdd3a85c0bd5e5d97e6fe8260af104144b9
Dylan_W <96798645+ddwalters@users.noreply.github.com>
2023-04-24 09:56:53 -0400

    fixed add contact not showing issue

--- Developer-Project/Developer-Project/App.xaml.cs
+++ Developer-Project/Developer-Project/App.xaml.cs
@@ -14,18 +14,5 @@ namespace Developer_Project
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            Phonebook contactslist = new Phonebook("Clayton");
-
-            contactslist.MakeContact(new Acquaintance("Dylan", "ddwalters987@gmail.com", "6162017277", "8491 South Maple Ct"));
-
-            contactslist.MakeContact(new Acquaintance("kira", "kirasEmail@gmail.com", "616-something-something", "next to vitales"));
-
-            IEnumerable<Acquaintance> contact = contactslist.GetAcquaintances("Dylan");
-            Console.WriteLine(contact);
-
-            base.OnStartup(e);
-        }
     }
 }
